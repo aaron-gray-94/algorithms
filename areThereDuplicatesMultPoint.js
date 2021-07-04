b7ae254d8d85bb524c4019ab55c81ca7b45f7048
aaron-gray-94 <61071650+aaron-gray-94@users.noreply.github.com>
2021-07-04 13:38:22 -0700

    Add files via upload

---
+++ areThereDuplicatesMultPoint.js
@@ -0,0 +1,14 @@
+function areThereDuplicatesMultPoint(...args) {
+    // Two pointers
+    args.sort((a,b) => a > b);
+    let start = 0;
+    let next = 1;
+    while(next < args.length){
+      if(args[start] === args[next]){
+          return true
+      }
+      start++
+      next++
+    }
+    return false
+  }
